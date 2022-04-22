113a151b8fd0e95d5a99badec7f533c1baadff32
Abdorabie11 <abdoelprince77889900@gmail.com>
2022-04-22 16:13:24 -0700

    update

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyC5EkbwZX2ifnJJWl2Jo__NY5bnBmOi2-A';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/search',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
