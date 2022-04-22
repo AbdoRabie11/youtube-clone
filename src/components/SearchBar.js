6979fb918e0a6489370bf0c14d0bf02fca9dea26
Abdorabie11 <abdoelprince77889900@gmail.com>
2022-04-22 14:32:52 -0700

    update

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import './SearchBar.css'
+import logo from '../assets/imgs/youtube.webp';
+export default class SearchBar extends Component {
+    state = { term: ''}
+    onChangeInput = (e) => {
+     this.setState({ term : e.target.value})
+    }
+    onFormSubmit = (e) => {
+        e.preventDefault()
+    }
+  render() {
+    return (
+        <div className="search-bar ui main__search">
+            <img src={logo} className="logo__img" alt=""   />
+            <form onSubmit={this.onFormSubmit} className="ui form">
+                <div className="field">
+                    <label></label>
+                    <input type="text" onChange={this.onChangeInput}  value={this.state.term}/>
+                </div>
+            </form>
+        </div>
+    )
+  }
+}
