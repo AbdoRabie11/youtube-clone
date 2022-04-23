5b016c84d326165e7e608640ba7703482a5ea582
Abdorabie11 <abdoelprince77889900@gmail.com>
2022-04-22 18:24:45 -0700

    update

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
-const KEY = 'AIzaSyC5EkbwZX2ifnJJWl2Jo__NY5bnBmOi2-A';
+const KEY = "AIzaSyC5EkbwZX2ifnJJWl2Jo__NY5bnBmOi2-A";
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3/search',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY
-    }
-})
+  baseURL: "https://www.googleapis.com/youtube/v3/search",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    type: "video",
+    key: KEY,
+  },
+});
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
-import './SearchBar.css'
-import logo from '../assets/imgs/youtube.webp';
+import "./SearchBar.css";
+import logo from "../assets/imgs/youtube.webp";
 export default class SearchBar extends Component {
-    state = { term: ''}
-    onChangeInput = (e) => {
-     this.setState({ term : e.target.value})
-    }
-    onFormSubmit = (e) => {
-        e.preventDefault()
-    }
+  state = { term: "" };
+  onChangeInput = (e) => {
+    this.setState({ term: e.target.value });
+  };
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+    
+  };
   render() {
     return (
-        <div className="search-bar ui main__search">
-            <img src={logo} className="logo__img" alt=""   />
-            <form onSubmit={this.onFormSubmit} className="ui form">
-                <div className="field">
-                    <label></label>
-                    <input type="text" onChange={this.onChangeInput}  value={this.state.term}/>
-                </div>
-            </form>
-        </div>
-    )
+      <div className="search-bar ui main__search">
+        <img src={logo} className="logo__img" alt="" />
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label></label>
+            <input
+              type="text"
+              onChange={this.onChangeInput}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
