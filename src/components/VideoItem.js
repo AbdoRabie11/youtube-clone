a5245718829ffae6087600dc689541cfc599aee0
Abdorabie11 <abdoelprince77889900@gmail.com>
2022-04-23 13:25:12 -0700

    update

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,18 +1,16 @@
 import React from "react";
-import './VideoItem.css'
-function VideoItem({ video }) {
+import "./VideoItem.css";
+function VideoItem({ video, onVideoSelect }) {
   return (
     <div className="ui middle aligned animated list">
-      <div className="video-item item">
+      <div onClick={() => onVideoSelect(video)} className="video-item item">
         <img
           className="ui image"
           src={video.snippet.thumbnails.medium.url}
           alt=""
         />
         <div className="content">
-            <div className="header">
-            {video.snippet.title}
-            </div>
+          <div className="header">{video.snippet.title}</div>
         </div>
       </div>
     </div>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,10 @@
 import React from "react";
 import VideoItem from "./VideoItem";
-function VideoList({ videos }) {
-  const renderdList =  videos.map((video) => {
-        return <VideoItem video={video}/>
-    })
+function VideoList({ videos, onVideoSelect }) {
+  const renderdList = videos.map((video) => {
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+  });
   return <div>{renderdList}</div>;
-
 }
 
 export default VideoList;
